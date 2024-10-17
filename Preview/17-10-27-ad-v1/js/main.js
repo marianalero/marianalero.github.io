53649b27666b59bc1b132eaea6385edecac36a21
Mariana Lerma <marianalermarodriguez@gmail.com>
2024-10-17 16:26:33 -0700

    tipografias

--- Preview/17-10-27-ad-v1/js/main.js
+++ Preview/17-10-27-ad-v1/js/main.js
@@ -321,23 +321,23 @@
 			selNumInv =0;
 		}
 
-		//https://docs.google.com/forms/d/e/1FAIpQLSfCvpbT2QsZEhVbUcPu2DcuS0wh6TkDuPh61WfRg9uJkaWa5g/viewform?usp=pp_url&entry.516140191=n&entry.827025270=t&entry.1599079301=c&entry.465259973=1
-	 	// let url = "https://docs.google.com/forms/d/e/1FAIpQLSfCvpbT2QsZEhVbUcPu2DcuS0wh6TkDuPh61WfRg9uJkaWa5g/formResponse?entry.516140191="+name+"&entry.827025270="+telefono+"&entry.1599079301="+confirmText+"&entry.465259973="+selNumInv+'&submit=Submit';
-		// console.log(url);
-		// fetch(url, { method: 'GET', 
-		// mode: "no-cors", // apparently Google will only submit a form if "mode" is "no-cors"
-		// redirect: "follow",
-		// referrer: "no-referrer",
-		// headers: {
-		//   'Content-Type': 'application/x-www-form-urlencoded',
-		// },
-		//  } )
-		// .then(res=> {
-		// 	console.log(res)
-		// 	var confirmModal = new bootstrap.Modal(document.getElementById('confirmModal'))
-		// 	confirmModal.show()
-		// })
-		// .catch(reason => console.log(reason));
+		//https://docs.google.com/forms/d/e/1FAIpQLSdwKuwp1CI6jCh4EbPklpKuezh_DXegDFiULdPyrOCPXON3dg/viewform?usp=pp_url&entry.516140191=mariana&entry.827025270=6663262%7C&entry.1599079301=si&entry.465259973=5
+	 	let url = "https://docs.google.com/forms/d/e/1FAIpQLSdwKuwp1CI6jCh4EbPklpKuezh_DXegDFiULdPyrOCPXON3dg/formResponse?entry.516140191="+name+"&entry.827025270="+telefono+"&entry.1599079301="+confirmText+"&entry.465259973="+selNumInv+'&submit=Submit';
+		console.log(url);
+		fetch(url, { method: 'GET', 
+		mode: "no-cors", // apparently Google will only submit a form if "mode" is "no-cors"
+		redirect: "follow",
+		referrer: "no-referrer",
+		headers: {
+		  'Content-Type': 'application/x-www-form-urlencoded',
+		},
+		 } )
+		.then(res=> {
+			console.log(res)
+			var confirmModal = new bootstrap.Modal(document.getElementById('confirmModal'))
+			confirmModal.show()
+		})
+		.catch(reason => console.log(reason));
 		
 	
 	}
@@ -381,6 +381,7 @@
 		inlineSVG();
 		bgVideo();
 	
+		$('#asistire').prop('checked',true);
 		var myModal = new bootstrap.Modal(document.getElementById('exampleModal'))
 		myModal.show()
 		
